d6b81a8c119fc49ad16e8e381900c7af4cfb8ba5
bdeLong <bendelong45@gmail.com>
2019-10-31 13:28:04 -0500

    Updates

---
+++ precache-manifest.79d4df10bb3bdcc5d59fe40b063db658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6bba24b50d882c4f675de61b3b9199",
+    "url": "/Developer-Portfolio/./index.html"
+  },
+  {
+    "revision": "1a482d639e53ff11a175",
+    "url": "/Developer-Portfolio/./static/css/main.2cf1872a.chunk.css"
+  },
+  {
+    "revision": "d37a35920b7009884804",
+    "url": "/Developer-Portfolio/./static/js/2.58436703.chunk.js"
+  },
+  {
+    "revision": "1a482d639e53ff11a175",
+    "url": "/Developer-Portfolio/./static/js/main.d7bcb16c.chunk.js"
+  },
+  {
+    "revision": "942f34830be437086df5",
+    "url": "/Developer-Portfolio/./static/js/runtime-main.6c6fe620.js"
+  }
+]);
